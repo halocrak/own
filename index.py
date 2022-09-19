a88fa6762132c129670ef303527ef680128f1a6c
MRX <79331796+halocrak@users.noreply.github.com>
2022-09-19 23:26:28 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import (
+    Flask
+)
+import requests
+import json
+app=Flask(__name__)
+@app.route('/user=<string:username>')
+def halo(username):
+    hedi= str(requests.get(f"https://www.snapchat.com//add/{username}").status_code)
+    mrx=(f"CODE : {hedi} USER : {username} CODE BY : i4m_mrx")
+    return mrx
+@app.route('/user/')
+def halom():
+    return "not found user"
+if __name__ =='__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=True
+    )
